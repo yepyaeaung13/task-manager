fa39444cb2688801c7e0d55793c7dcb56d965e97
yepyaeaung.mhs <yepyaeaung.mhs@gmail.com>
2025-06-02 01:19:44 +0630

    YP: test login issue

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,15 +1,16 @@
 import { getNewAccessToken } from "../utils/utils";
 import { getUserInfo } from "../services/googleAuth.service";
-import GoogleLogo from "../components/icons/GoogleLogo";
-import { useState } from "react";
-import IconLoading from "../components/icons/IconLoading";
+// import GoogleLogo from "../components/icons/GoogleLogo";
+// import { useState } from "react";
+// import IconLoading from "../components/icons/IconLoading";
+import { GoogleLogin } from "@react-oauth/google";
 
 export default function SignIn() {
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
   const loginWithGoogle = async () => {
     try {
       const accessToken = await getNewAccessToken();
-      setLoading(true);
+      // setLoading(true);
       localStorage.setItem("google_access_token", accessToken);
       const userInfo = await getUserInfo();
       localStorage.setItem("user_info", JSON.stringify(userInfo));
@@ -18,21 +19,22 @@ export default function SignIn() {
       console.error("Failed to get access token:", err);
       return;
     }
-    setLoading(false);
+    // setLoading(false);
   };
 
   return (
     <div className="h-[100dvh] flex justify-center items-center bg-gray-100">
       <div className="flex flex-col justify-between p-10 rounded-xl shadow-xl bg-white md:w-96 w-full md:h-96 h-full text-center">
         <h1 className="text-2xl font-bold mb-4">Hi, Welcome Back! 👋</h1>
-        <button
+        {/* <button
           className="w-full bg-secondary flex items-center justify-center gap-3 border border-border-color px-2 py-2 rounded-lg cursor-pointer hover:border-gray-500 "
           onClick={loginWithGoogle}
         >
           <GoogleLogo />
           Sign in with Google
           {loading && <IconLoading />}
-        </button>
+        </button> */}
+        <GoogleLogin onSuccess={loginWithGoogle} />
         <div className="font-bold flex justify-between">
           <p>Don’t have an account?</p>
           <a href="/signup" className="text-primary-text underline">
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,7 @@
 import { getNewAccessToken } from "../utils/utils";
 import { getUserInfo } from "../services/googleAuth.service";
-import GoogleLogo from "../components/icons/GoogleLogo";
+// import GoogleLogo from "../components/icons/GoogleLogo";
+import { GoogleLogin } from "@react-oauth/google";
 
 export default function SignUp() {
   const loginWithGoogle = async () => {
@@ -21,13 +22,14 @@ export default function SignUp() {
     <div className="h-[100dvh] flex justify-center items-center bg-gray-100">
       <div className="flex flex-col justify-between p-10 rounded-xl shadow-xl bg-white md:w-96 w-full md:h-96 h-full text-center">
         <h1 className="text-2xl font-bold mb-4">Create an account</h1>
-        <button
+        {/* <button
           className="w-full bg-secondary flex items-center justify-center gap-3 border border-border-color px-2 py-2 rounded-lg cursor-pointer hover:border-gray-500 "
           onClick={loginWithGoogle}
         >
           <GoogleLogo />
           Sign up with Google
-        </button>
+        </button> */}
+        <GoogleLogin onSuccess={loginWithGoogle} />
         <div className="font-bold flex justify-between">
           <p>Already have an account?</p>
           <a href="/" className="text-primary-text underline">
