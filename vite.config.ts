d172f6e6ca3692ef95c87cf4e65576d744dd8dba
yepyaeaung.mhs <yepyaeaung.mhs@gmail.com>
2025-06-01 23:57:36 +0630

    YP: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '192.168.92.75.nip.io',
-    port: 5173,
-  },
+  // server: {
+  //   host: '192.168.92.75.nip.io',
+  //   port: 5173,
+  // },
   plugins: [
     react({
       babel: {
